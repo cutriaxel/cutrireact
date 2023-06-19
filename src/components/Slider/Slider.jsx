c39b173f4cf1541d6083dc49ca3b1be6c5ce58b4
axelcutri <cutriaxel@hotmail.com>
2023-06-19 18:26:52 -0300

    formateo del documento

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -7,7 +7,7 @@ const Slider = () => {
   return (
     <motion.div className='slider-container'>
         <motion.div className='slider' drag='x' 
-        dragConstraints={{right: 0, left:-2123}} >
+        dragConstraints={{right: 0, left:-1100}} >
         {images.map(image => (
             <motion.div className='item'>
                 <img src={image} alt="" />
