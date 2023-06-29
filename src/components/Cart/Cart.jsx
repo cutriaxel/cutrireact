e4668f60a8587489c829da648af398c78ce70170
axelcutri <cutriaxel@hotmail.com>
2023-06-28 22:53:31 -0300

    comence a darle estilo css al cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -16,54 +16,54 @@ const Cart = () => {
       <hr />
 
       {cart.length > 0 ? (
-        cart.map((prod) => (
-          
-          <div className='producto-container' key={prod.id}>
-            <div className='producto-imagen'>
-              <img src={prod.image} alt={prod.title} />
+        <div className="row"> {/* Contenedor principal */}
+          <div className="col-md-8"> {/* Columna para los productos */}
+            {cart.map((prod) => (
+              <div className='producto-container' key={prod.id}>
+                <div className='producto-imagen'>
+                  <img src={prod.image} alt={prod.title} />
+                </div>
+                <div className='producto-info'>
+                  <div className='producto-titulo'>
+                    <h4>{prod.title}</h4>
+                  </div>
+                  <div className='col-md-2 producto-precio'>
+                    <p>Precio: ${prod.price}</p>
+                    <button className="btn btn-danger" onClick={() => eliminarDelCarrito(prod.id)}>
+                      <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                  </div>
+                 
+                </div>
+                <hr />
+              </div>
+            ))}
+          </div>
+          <div className='col-md-3'> {/* Columna para el resumen de compra */}
+            <div className='container-compra'>
+              <div className='total-compra'>
+                <h5>Total: ${totalCompra()}</h5>
+              </div>
+              <hr />
+              <div className='btn-compra'>
+                <div className='btn-pagar'>
+                  <button onClick={vaciarCarrito} className="btn btn-success">
+                    Ir a pagar
+                  </button>
+                </div>
+                <div className='btn-vaciar'>
+                  <button onClick={vaciarCarrito} className="btn btn-danger">
+                    Vaciar carrito
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className='producto-info'>
-              <div className='producto-titulo'>
-                <h4>{prod.title}</h4>
-              </div>
-              <div className='producto-precio'>
-                <p>Precio: ${prod.price}</p>
-              </div>
-              <div className='producto-eliminar-carrito'>
-                <button className="btn btn-danger" onClick={() => eliminarDelCarrito(prod.id)}>
-                  <FontAwesomeIcon icon={faTrash} />
-                </button>
-              </div>
-           
-            <hr />
           </div>
-          </div>
-        ))
+        </div>
       ) : (
         <div className="carrito-vacio">
           <p>El carrito está vacío.</p>
           <button className="btn btn-primary">Ir a productos</button>
-        </div>
-      )}
-
-      {cart.length > 0 && (
-        <div className='container-compra'>
-          <div className='total-compra'>
-            <h5>Total: ${totalCompra()}</h5>
-          </div>
-          <hr />
-          <div className='btn-compra'>
-            <div className='btn-pagar'>
-              <button onClick={vaciarCarrito} className="btn btn-success">
-                Ir a pagar
-              </button>
-            </div>
-            <div className='btn-vaciar'>
-              <button onClick={vaciarCarrito} className="btn btn-danger">
-                Vaciar carrito
-              </button>
-            </div>
-          </div>
         </div>
       )}
     </div>
