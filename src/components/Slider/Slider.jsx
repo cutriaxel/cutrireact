a4fa7860c9738fc1afa769fd02e06b5ec005cc07
axelcutri <cutriaxel@hotmail.com>
2023-07-07 16:34:32 -0300

    formateo del documento

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import images from '../../helpers/image';
 import './slider.scss';
 import { motion } from 'framer-motion';
@@ -6,17 +6,15 @@ import { motion } from 'framer-motion';
 const Slider = () => {
   return (
     <motion.div className='slider-container'>
-        <motion.div  className='slider' drag='x' 
-        dragConstraints={{right: 0, left:-1100}} >
-        {images.map(image => (
-            <motion.div className='item' key={image.id} >
-                <img src={image} alt="" />
-            </motion.div>
+      <motion.div className='slider' drag='x' dragConstraints={{ right: 0, left: -1100 }}>
+        {images.map((image) => (
+          <motion.div className='item' key={image.id}>
+            <img src={image.src} alt='' />
+          </motion.div>
         ))}
-        </motion.div>
-        
+      </motion.div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
