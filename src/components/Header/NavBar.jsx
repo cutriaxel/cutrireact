c8e78c2b9d4add85f23ddb7c775d58c9644d9639
axelcutri <cutriaxel@hotmail.com>
2023-07-09 20:11:38 -0300

    agregue iconos de material ui a mi navbar

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -4,13 +4,15 @@ import CartWidget from '../CartWidget/CartWidget';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Home, Email } from '@mui/icons-material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import { FaBars, FaTimes } from 'react-icons/fa';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../../Context/CartContext';
-import { AuthContext } from '../../Context/AuthContext'
-
+import { AuthContext } from '../../Context/AuthContext';
+import { FaMicrochip, FaKeyboard } from 'react-icons/fa';
+import { MdDesktopMac } from 'react-icons/md';
 
 function NavBar({ handleOpenModal, variant = false }) {
   const { user, logout } = useContext(AuthContext)
@@ -25,27 +27,33 @@ function NavBar({ handleOpenModal, variant = false }) {
   return (
     <header>
       <Navbar bg="dark" expand="lg" className="navbar__container">
-
         <Container style={{ display: 'contents' }}>
-
           <Navbar.Toggle aria-controls="navbarNav" />
-
-          <Navbar.Collapse id="navbarNav" >
+          <Navbar.Collapse id="navbarNav">
             <div className='cutritech-title'>
-              <h2>cutri<spam  className="logo-highlight">Tech</spam></h2>
+              <h2>cutri<spam className="logo-highlight">Tech</spam></h2>
             </div>
             <Nav className="nav__link">
-              <Link className='nav__link' to={"/"} >Inicio</Link>
-              <Link className='nav__link' to={"/productos"} >Productos</Link>
-              <Link className='nav__link' to={"/Contacto"} >Contacto</Link>
-              <Link className='nav__link' to={"/Cart"} >Mi carrito</Link>
+              <Link className='nav__link' to={"/"} >
+                <Home className='iconos-nav' /> Inicio
+              </Link>
+              <Link className='nav__link' to={"/productos"} >
+                <ShoppingCartIcon className='iconos-nav' /> Productos
+              </Link>
+              <Link className='nav__link' to={"/Contacto"} >
+                <Email className='iconos-nav' /> Contacto
+              </Link>
+              <Link className='nav__link' to={"/Cart"} >
+                <ShoppingCartIcon className='iconos-nav' /> Mi carrito
+              </Link>
             </Nav>
             <div>
-              <p style={{color: '#01f603', fontSize: '20px', fontFamily: 'Cambria, Cochin, Georgia, Times, "Times New Roman", serif'}}>Bienvenido: {user.email}</p>
+              <p style={{ color: '#01f603', fontSize: '20px', fontFamily: 'Cambria, Cochin, Georgia, Times, "Times New Roman", serif' }}>Bienvenido: {user.email}</p>
               <div className='cart-widget__count' onClick={handleOpenModal}>
                 <CartWidget cartCount={cart.length} />
                 <button className='btn cerrar_sesion' onClick={logout}>
-                  <FontAwesomeIcon icon={faSignOutAlt} onClick={logout} />
+                  <spam style={{ color: 'white' }}>Cerrar sesión</spam>
+                  <ExitToAppIcon style={{ color: 'green', marginLeft: '5px' }} />
                 </button>
               </div>
             </div>
@@ -66,13 +74,43 @@ function NavBar({ handleOpenModal, variant = false }) {
         {showSidebar && (
           <div className="sidebar">
             <ul className="sidebar__list">
-              <li><Link className='nav__link' to={"/productos/Computadoras"} >ARMA TU PC</Link></li>
-              <li><Link className='nav__link' to={"/"} >ARMA TU COMBO</Link></li>
+              <li>
+                <Link className='nav__link' to={"/productos/Computadoras"} >
+                  <MdDesktopMac className='iconos-sidebar' />
+                  ARMA TU PC
+                </Link>
+              </li>
+              <li>
+                <Link className='nav__link' to={"/"} >
+                  <FaMicrochip className='iconos-sidebar' />
+                  ARMA TU COMBO
+                </Link>
+              </li>
               <hr style={{ color: 'white' }} />
-              <li><Link className='nav__link' to={"/productos/Monitores"} >Monitores</Link></li>
-              <li> <Link className='nav__link' to={"/productos/Procesadores"} >Procesadores</Link></li>
-              <li><Link className='nav__link' to={"/productos/Perifericos"} >Perifericos</Link></li>
-              <li><Link className='nav__link' to={"/productos/Computadoras"} >Computadoras</Link></li>
+              <li>
+                <Link className='nav__link' to={"/productos/Monitores"} >
+                  <MdDesktopMac className='iconos-sidebar' />
+                  Monitores
+                </Link>
+              </li>
+              <li>
+                <Link className='nav__link' to={"/productos/Procesadores"} >
+                  <FaMicrochip className='iconos-sidebar' />
+                  Procesadores
+                </Link>
+              </li>
+              <li>
+                <Link className='nav__link' to={"/productos/Perifericos"} >
+                  <FaKeyboard className='iconos-sidebar' />
+                  Periféricos
+                </Link>
+              </li>
+              <li>
+                <Link className='nav__link' to={"/productos/Computadoras"} >
+                  <MdDesktopMac className='iconos-sidebar' />
+                  Computadoras
+                </Link>
+              </li>
             </ul>
           </div>
         )}
